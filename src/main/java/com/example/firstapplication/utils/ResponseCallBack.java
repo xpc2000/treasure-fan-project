7c459d7d1d67c9876ba99ec8c6bdebfabbc93f98
xpc2000 <61380068+xpc2000@users.noreply.github.com>
2021-11-29 21:43:10 +0800

    Delete src directory

--- src/main/java/com/example/firstapplication/utils/ResponseCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.example.firstapplication.utils;
-
-import org.json.JSONException;
-
-public interface ResponseCallBack {
-    void success(String json) throws JSONException;
-
-    void error(String json);
-}
